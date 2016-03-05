25c0a870705865272e10cffa4ebcc137e398db90
x14322731 <briandoyle884@gmail.com>
2016-03-05 17:37:25 +0000

    update with issue to be resolved

--- transaction-page/transaction.php
+++ transaction-page/transaction.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE HTML>
 <html> 
 <head>
@@ -6,7 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://www.paypalobjects.com/js/external/apdg.js" type="text/javascript"></script>
     <title>Landing Page - Start Bootstrap Theme</title>
 
     <!-- Bootstrap Core CSS -->
@@ -29,13 +31,12 @@
 
 </head>
 <body>
-   <script src="CreditCardvalidation.js"></script>
 
     <!-- Navigation -->
     <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
         <div class="container topnav">
             <!-- Brand and toggle get grouped for better mobile display -->
-s            <div class="navbar-header">
+            <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
@@ -68,10 +69,11 @@ s            <div class="navbar-header">
     <a name="about"></a>
     <div class="intro-header">
         <div class="container">
-<h1>Sign Up</h1>
- 
-<?php echo $_POST["name"]; ?>
-<form action="connect.php" method="post">
+
+<form action="https://www.sandbox.paypal.com/webapps/adaptivepayment/flow/pay" target="PPDGFrame" class="standard">
+
+                       
+</br>
 Title:<select>
   <option value="Mr.">Mr.</option>
   <option value="Mrs.">Mrs.</option>
@@ -80,12 +82,119 @@ Title:<select>
 </select>Name:<input type="text" name="name"> <br>
 
 </br>
-Card Type:<input type="text" name="card type"><br>
+
+Card Type :<select tabindex="5" id="CardType" style="margin-left: 10px;">
+ <option value="VISA" name="VISA">Visa</option>
+  <option value="American Express" name="American Express">American Express.</option>
+  <option value="Mastercard" name="MasterCard">Mastercard</option>
+   <option value="Maestro" name="Maestro">Maestro</option>
+    <option value="LaserCard"name="LaserCard">Laser</option>
+  </select>
+  </br>
+  </br>
+  Credit Card Number :<input type="text" name="cardnumber" id="cardnumber" placeholder="1234 5678 9012 3456">
 </br>
-Card Number:<input type="text" name="card_number" id="card_number"><br>
+</br> 
+Billing Address line 1:<input type="text" name="address1"><br>
+</br>
+Billing Address line 2:<input type="text" name="address2"><br>
+</br>
+Post code:<input type="text" name="postcode"><br>
+</br>
+County:<select>
+  <option value="Antrim">Antrim</option>
+  <option value="Armagh">Armagh</option>
+  <option value="Carlow">Carlow</option>
+  <option value="Cavan">Cavan</option>
+<option value="Clare">Clare</option>
+  <option value="Cork">Cork</option>
+  <option value="Donegal">Donegal</option>
+  <option value="Down">Down</option>
+<option value="Dublin">Dublin</option>
+  <option value="Fermanagh">Fermanagh</option>
+  <option value="Galway">Galway</option>
+  <option value="Kerry">Kerry</option>
+<option value="Kildare">Kildare</option>
+  <option value="Kilkenny">Kilkenny</option>
+  <option value="Laois">Laois</option>
+  <option value="Leitrim">Leitrim</option>
+<option value="Limerick">Limerick</option>
+  <option value="Londonderry">Londonderry</option>
+  <option value="Longford">Longford</option>
+  <option value="Louth">Louth</option>
+<option value="Mayo">Mayo</option>
+  <option value="Meath">Meath</option>
+  <option value="Monaghan">Monaghan</option>
+  <option value="Offaly">Offaly</option>
+<option value="Roscommon">Roscommon</option>
+  <option value="Sligo">Sligo</option>
+  <option value="Tipperary">Tipperary</option>
+  <option value="Tyrone">Tyrone</option>
+<option value="Waterford">Waterford</option>
+  <option value="Westmeath">Westmeath</option>
+  <option value="Wexford">Wexford</option>
+  <option value="Wicklow">Wicklow</option>
+</select><br>
+</br>
+Enter Image Text
 
-<input type="submit">
+
+
+<input name="captcha" type="text">
+<img src="captcha.php" /><br>
+</br>
+
+<input type="image" id="submitBtn" value="Pay with PayPal" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif">
+<input id="type" type="hidden" name="expType" value="mini">
+<input id="paykey" type="hidden" name="paykey" value="insert_pay_key">
 </form>
+<script type="text/javascript" charset="utf-8">
+var dgFlowMini = new PAYPAL.apps.DGFlowMini({trigger: 'submitBtn'});
+</script>
 
+
+
+
+ </div>
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+
+<footer>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <ul class="list-inline">
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li class="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="#about">About</a>
+                        </li>
+                        <li class="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="#services">Services</a>
+                        </li>
+                        <li class="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                    <p class="copyright text-muted small">Copyright &copy; Your Company 2014. All Rights Reserved</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
+
 </html>
